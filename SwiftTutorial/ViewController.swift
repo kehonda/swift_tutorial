396ca33db93a81bfbb2f75a9d1e9f2981ef473a6
kehonda <731.kentaro@gmail.com>
2019-02-19 20:31:51 +0900

    Implement a Custom Controll

--- SwiftTutorial/ViewController.swift
+++ SwiftTutorial/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,11 +62,5 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
     }
-    
-    @IBAction func setDefaultLabelText(_ sender: Any) {
-        mealNameLabel.text = "Default Text"
-    }
-    
-
 }
 
