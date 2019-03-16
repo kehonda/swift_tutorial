ef980e52644e1a91f8929931aee9ab66b399a21f
kehonda <731.kentaro@gmail.com>
2019-03-17 08:19:14 +0900

    Persist Data (in progress)

--- SwiftTutorial/MealTableViewController.swift
+++ SwiftTutorial/MealTableViewController.swift
@@ -104,6 +104,16 @@ class MealTableViewController: UITableViewController {
         meals += [meal1, meal2, meal3]
     }
     
+    private func saveMeals() {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path)
+        
+        if isSuccessfulSave {
+            os_log("Meals successfully saved.", log: OSLog.default, type: .debug)
+        } else {
+            os_log("Failed to save meals...", log: OSLog.default, type: .error)
+        }
+    }
+    
     //MARK: - Navigation
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
